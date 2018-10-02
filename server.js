7317c225df08eb6b6edf4fda771bb2e66c940693
jhaff <jacobhaff2@gmail.com>
2018-10-02 11:49:06 -0700

    Create route written for grateful nugget.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 var exphbs = require('express-handlebars');
 const bodyParser = require('body-parser'); // INITIALIZE BODY-PARSER AND ADD IT TO APP
+const GratefulNugget = require('../models/grateful-nugget.js')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -13,3 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/gratefulness-project';
 mongoose.connect(mongoUri, { useNewUrlParser: true });
+
+// CREATE
+app.post('/grateful-nuggets', (req, res) => {
+    console.log(req.body);
+    GratefulNugget.create(req.body)
+        .then(nugget => {
+            console.log(nugget)
+        }).catch(error => {
+            console.log(error.message);
+        });
+});
